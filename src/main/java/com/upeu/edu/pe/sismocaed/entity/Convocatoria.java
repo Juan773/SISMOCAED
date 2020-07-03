adcb39ec451e91a762678002f31d466c484d712a
Alejandro <alejandrogutierrez@upeu.edu.pe>
2020-07-03 05:06:03 -0500

    commit alejo 1

--- src/main/java/com/upeu/edu/pe/sismocaed/entity/Convocatoria.java
+++ src/main/java/com/upeu/edu/pe/sismocaed/entity/Convocatoria.java
@@ -13,7 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="convicatoria")
+@Table(name="convocatoria")
 public class Convocatoria implements Serializable{
 
 	private static final long serialVersionUID = 203527103058157394L;

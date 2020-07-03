4f4790f79c387c298d010fa470479f5ee5ad4fb2
Juan773 <juanquezada@upeu.edu.pe>
2020-07-03 01:48:13 -0500

    raaaaa

---
+++ src/main/java/com/upeu/edu/pe/sismocaed/SismocaedApplication.java
@@ -0,0 +1,13 @@
+package com.upeu.edu.pe.sismocaed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismocaedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismocaedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upeu/edu/pe/sismocaed/SismocaedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upeu.edu.pe.sismocaed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SismocaedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
